363eb7b16ff5b3bac0f3fa9e1fc2351a45a01b42
Honza Kubita <honza.kubita@gmail.com>
2023-04-03 18:45:47 +0200

    Hotfix removed "/" from the end of BASE_URL Removed for easier path url joining

--- modules/http.js
+++ modules/http.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const BASE_URL = 'https://www.spsejecna.cz/';
+const BASE_URL = 'https://www.spsejecna.cz';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL
