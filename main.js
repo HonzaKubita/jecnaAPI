6f22fc365aebe3feaf0a8092e6de3837543f2f73
RORAKUS <rorakcodes@gmail.com>
2023-04-07 16:08:20 +0200

    created main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const autoRestAPI = require('autorestapi');
+const {constants} = require("./modules/constants");
+const exceptionHandler = require("exceptions/exceptionHandler");
+
+const app = express();
+app.use(express.json()); // Parse json body
+app.use(exceptionHandler); // Exception handling
+
+autoRestAPI(app); // Register endpoints with autoRestAPI
+
+app.listen(constants.server.port, () => { // Start the server
+    console.log(`Server running on port ${constants.server.port}!`);
+});
