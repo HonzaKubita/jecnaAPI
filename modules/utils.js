8d3c13bd9e4726cc230480b5737f3d7ebd1f0fff
RORAKUS <rorakcodes@gmail.com>
2023-04-08 16:17:15 +0200

    added utils.js

---
+++ modules/utils.js
@@ -0,0 +1,32 @@
+const ServerException=  require('../exceptions/server/serverException');
+const {JSDOM} = require('jsdom');
+
+/**
+ * Returns a cookie value from set-cookie header
+ * @param name{string} The name of the cookie
+ * @param resHeaders{AxiosHeaders} The headers object from the response
+ * @return {string} The value of the cookie
+ */
+function getCookie(name, resHeaders) {
+    let header = resHeaders["set-cookie"];
+    if (header === undefined) throw new ServerException("Response from jecna server doesn't contain the Set-Cookie header.");
+
+    const foundCookie = header.find(cookie => cookie.includes(name));
+    if (foundCookie === undefined) throw new ServerException(`The Set-Cookie header does not contain a cookie with name ${name}.`);
+
+    return foundCookie?.match(new RegExp(`^${name}=(.+?);`))?.[1];
+}
+
+/**
+ * Returns the document object from the raw response data
+ * @param responseData{string} The data
+ * @returns {Document} The document object
+ */
+function documentOf(responseData) {
+    return new JSDOM(responseData).window.document;
+}
+
+module.exports = {
+    getCookie,
+    documentOf
+}
