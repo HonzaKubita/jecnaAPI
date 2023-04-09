be59ee2fe9db789dbe1064c26b37adbd34f79f99
RORAKUS <rorakcodes@gmail.com>
2023-04-09 15:36:27 +0200

    added /school/event endpoint

---
+++ api/school/event.js
@@ -0,0 +1,20 @@
+const {payloadIsType, tokenValid, siteFound} = require("../../modules/checker");
+const {getSafeField, getSafeNumberField} = require("../../modules/utils");
+const {constants} = require("../../modules/constants");
+const {jecnaAuthRequest} = require("../../modules/http");
+const {eventParser} = require("../../parsers/school/eventParser");
+module.exports = {
+    post: async (req, res) => {
+        payloadIsType(req.headers);
+
+        const token = getSafeField(req.body.token, "token", constants.jecna.wrongToken);
+        const eventCode = getSafeNumberField(req.body.eventCode, "eventCode");
+
+        const eventRes = await jecnaAuthRequest(`/akce/${eventCode}`, token);
+        if (req.body.token !== undefined) tokenValid(eventRes.data);
+        siteFound(eventRes.data, `Event ${eventCode}`);
+
+        const eventJSON = eventParser(eventRes.data);
+        res.status(200).json(eventJSON);
+    }
+}
