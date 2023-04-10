07b355e146959c28164a97db9cd002de68debed0
RORAKUS <rorakcodes@gmail.com>
2023-04-10 14:44:16 +0200

    added /absence endpoint

---
+++ api/user/absence.js
@@ -0,0 +1,20 @@
+const {payloadIsType, tokenValid, siteFound} = require("../../modules/checker");
+const {getSafeStringField, getSafeNumberField} = require("../../modules/utils");
+const {jecnaAuthRequest} = require("../../modules/http");
+const {absenceParser} = require("../../parsers/user/absenceParser");
+module.exports = {
+    post: async (req, res) => {
+        payloadIsType(req.headers);
+
+        const token = getSafeStringField(req.body.token, "token");
+        const year = getSafeNumberField(req.body.year, "year", -1);
+
+        const absenceLink = `/absence/student${year === -1 ? "" : `?schoolYearId=${year}`}`;
+        const absenceRes = await jecnaAuthRequest(absenceLink, token);
+        tokenValid(absenceRes.data);
+        siteFound(absenceRes.data, `Year ${year}`);
+
+        const absenceJSON = absenceParser(absenceRes.data);
+        res.status(200).json(absenceJSON);
+    }
+}
---
+++ parsers/user/absenceParser.js
@@ -0,0 +1,50 @@
+const {documentOf} = require("../../modules/utils");
+
+function absenceParser(htmlBody) {
+    const absenceDOM = documentOf(htmlBody);
+    const absenceJSON = {
+        absence: [],
+        years: []
+    };
+
+    // parse absence
+    const absenceTbody = absenceDOM
+        .getElementsByClassName("absence-list")[0] // absenceTable
+        ?.children?.[0];
+
+    // parse years
+    const yearSelect = absenceDOM.getElementById("schoolYearId");
+    for (const yearOption of yearSelect.children) {
+        absenceJSON.years.push({
+            name: yearOption.innerHTML,
+            id: Number(yearOption.value)
+        });
+    }
+
+    if (absenceTbody === undefined) return absenceJSON;
+
+    for (const absenceTr of absenceTbody.children) {
+        const absenceDateSplit = absenceTr.getElementsByClassName("nounderline")[0].innerHTML.split(" ");
+
+        const absenceDate = absenceDateSplit[0];
+        const absenceWeekDay = absenceDateSplit[1].replaceAll(/[()]/g, "");
+        const absenceHours = Number(
+            absenceTr.getElementsByClassName("count")[0]
+                .children[0]
+                .innerHTML
+                .replaceAll(/hodin[ya]?/g, "")
+        ); // Number(hoursTd.hoursStrong.innerHTML)
+
+        absenceJSON.absence.push({
+            date: absenceDate,
+            weekDay: absenceWeekDay,
+            hours: absenceHours
+        });
+    }
+
+    return absenceJSON;
+}
+
+module.exports = {
+    absenceParser
+}
