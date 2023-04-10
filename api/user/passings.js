d68e1c78f3bb44a51af8afcd625ce572c15dd6e7
RORAKUS <rorakcodes@gmail.com>
2023-04-10 15:26:18 +0200

    added /user/passings endpoint

---
+++ api/user/passings.js
@@ -0,0 +1,26 @@
+const {payloadIsType, tokenValid, siteFound} = require("../../modules/checker");
+const {getSafeStringField, getSafeNumberField} = require("../../modules/utils");
+const {jecnaAuthRequest} = require("../../modules/http");
+const {passingsParser} = require("../../parsers/user/passingsParser");
+module.exports = {
+    post: async (req, res) => {
+        payloadIsType(req.headers);
+
+        const token = getSafeStringField(req.body.token, "token");
+        const year = getSafeNumberField(req.body.year, "year", -1);
+        const month = getSafeNumberField(req.body.month, "month", -1);
+
+        const passingsLink =
+            "/absence/passing-student" +
+            (year !== -1 || month !== -1 ? "?" : "") +
+            (year !== -1 ? `schoolYearId=${year}` : "") +
+            (year !== -1 && month !== -1 ? "&" : "") +
+            (month !== -1 ? `schoolYearPartMonthId=${month}` : "");
+        const passingsRes = await jecnaAuthRequest(passingsLink, token);
+        tokenValid(passingsRes.data);
+        siteFound(passingsRes.data, `${year !== -1 ? `Year ${year}` : ""}${year !== -1 && month !== -1 ? " or " : ""}${month !== -1 ? `Month ${month}` : ""}`);
+
+        const passingsJSON = passingsParser(passingsRes.data);
+        res.status(200).json(passingsJSON);
+    }
+}
---
+++ parsers/user/passingsParser.js
@@ -0,0 +1,56 @@
+const {documentOf} = require("../../modules/utils");
+
+function passingsParser(htmlBody) {
+    const passingsDOM = documentOf(htmlBody);
+    const passingsJSON = {
+        passings: [],
+        years: [],
+        months: []
+    };
+
+    // parse years
+    const yearSelect = passingsDOM.getElementById("schoolYearId");
+    for (const yearOption of yearSelect.children) {
+        passingsJSON.years.push({
+            name: yearOption.innerHTML,
+            id: Number(yearOption.value)
+        });
+    }
+    // parse months
+    const monthSelect = passingsDOM.getElementById("schoolYearPartMonthId");
+    for (const monthOption of monthSelect.children) {
+        passingsJSON.months.push({
+            name: monthOption.innerHTML,
+            id: Number(monthOption.value)
+        });
+    }
+    // parse passings
+    const passingsTbody = passingsDOM
+        .getElementsByClassName("absence-list")[0] // passingsTable
+        ?.children?.[0];
+
+    if (passingsTbody === undefined) return passingsJSON;
+
+    for (const passingTr of passingsTbody.children) {
+        const passingDateSplit = passingTr.children[0].innerHTML.split("&nbsp;");
+        const passingValueMatch = passingTr.children[1].children[0]?.innerHTML?.match(/Příchod (\d{1,2}:\d{2}), Odchod (\d{1,2}:\d{2})/);
+
+        const passingDate = passingDateSplit[0];
+        const passingWeekDay = passingDateSplit[1].replaceAll(/[()]/g, "");
+        const passingArrived = passingValueMatch?.[1] ?? "";
+        const passingLeft = passingValueMatch?.[2] ?? "";
+
+        passingsJSON.passings.push({
+            date: passingDate,
+            weekDay: passingWeekDay,
+            arrived: passingArrived,
+            left: passingLeft
+        });
+    }
+
+    return passingsJSON;
+}
+
+module.exports = {
+    passingsParser
+}
